b688e5e9e382b1295c656deadd1aba2267844111
Shtefan Nuzhdenko <s-nuzhdenko@mail.ru>
2021-07-26 11:57:41 -0500

    Test case Mixed is done

--- src/day1/ResetPractice.java
+++ src/day1/ResetPractice.java
@@ -5,4 +5,7 @@ public class ResetPractice{
 // part1 is done
     // part2 is done
     // part3 is done
+    // part4 is done
+    // part5 is done
+    // Mixed | Soft reset remove teh commit from history but keep the local changes
 }
