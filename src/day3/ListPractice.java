e96a6bf297885032bfc7be7ec87b06874e89adca
Shtefan Nuzhdenko <s-nuzhdenko@mail.ru>
2021-07-29 13:33:47 -0500

    added an item

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,4 +1,12 @@
 package day3;
 
+import java.util.ArrayList;
+
 public class ListPractice {
+
+    public static void main(String[] args) {
+
+       // List<String> lst = new ArrayList<>();
+        // adding one item
+    }
 }
